eb1372d355eb8d2066ce994668acc63c7a5517c2
andrewmackenzie <andrew@capstannetworks.com>
2017-08-07 23:19:59 -0500

    Update osname.sh

--- osname.sh
+++ osname.sh
@@ -1,4 +1,5 @@
-#OS Release Lists
+# OS Release Lists
+# (No doubt there are more mature bash-y ways of doing this, but I'll understand this next time I look at it
 OSProductPrefixList=('Mac OS X' 'Mac OS X' 'Mac OS X' 'Mac OS X' 'Mac OS X' 'Mac OS X' 'Mac OS X' 'Mac OS X' 'OS X' 'OS X' 'OS X' 'OS X' macOS macOS)
 OSCodeNameList=(Cheetah Puma Jaguar Panther Tiger Leopard 'Snow Leopard' Lion 'Mountain Lion' Mavericks Yosemite 'El Capitan' Sierra 'High Sierra')
 
