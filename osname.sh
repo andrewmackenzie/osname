9b068953c6e62fa777bb10bafa65385da4d45cd6
andrewmackenzie <andrew@capstannetworks.com>
2017-08-07 23:14:48 -0500

    Create osname.sh

---
+++ osname.sh
@@ -0,0 +1,10 @@
+#OS Release Lists
+OSProductPrefixList=('Mac OS X' 'Mac OS X' 'Mac OS X' 'Mac OS X' 'Mac OS X' 'Mac OS X' 'Mac OS X' 'Mac OS X' 'OS X' 'OS X' 'OS X' 'OS X' macOS macOS)
+OSCodeNameList=(Cheetah Puma Jaguar Panther Tiger Leopard 'Snow Leopard' Lion 'Mountain Lion' Mavericks Yosemite 'El Capitan' Sierra 'High Sierra')
+
+# show currently running version
+installedMajorRelease=$(sw_vers -productVersion | awk -F '.' '{ print $2 }')
+echo "You're on ${OSProductPrefixList[$installedMajorRelease]} ${OSCodeNameList[$installedMajorRelease]}"
+
+# sanity check - show them all
+for i in `seq 0 13`; do echo "10.${i}: ${OSProductPrefixList[$i]} ${OSCodeNameList[$i]}" ; done
