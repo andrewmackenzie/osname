c5e0181b38faa9081009ff14dbda485a0bba119e
Andrew MacKenzie <andrew@capstannetworks.com>
2018-09-10 11:27:50 -0500

    Update osname.sh

--- osname.sh
+++ osname.sh
@@ -1,6 +1,6 @@
 # OS Release Lists
 # (No doubt there are more mature bash-y ways of doing this, but I'll understand this next time I look at it
-OSProductPrefixList=('Mac OS X' 'Mac OS X' 'Mac OS X' 'Mac OS X' 'Mac OS X' 'Mac OS X' 'Mac OS X' 'Mac OS X' 'OS X' 'OS X' 'OS X' 'OS X' macOS macOS)
+OSProductPrefixList=('Mac OS X' 'Mac OS X' 'Mac OS X' 'Mac OS X' 'Mac OS X' 'Mac OS X' 'Mac OS X' 'Mac OS X' 'OS X' 'OS X' 'OS X' 'OS X' macOS macOS macOS)
 OSCodeNameList=(Cheetah Puma Jaguar Panther Tiger Leopard 'Snow Leopard' Lion 'Mountain Lion' Mavericks Yosemite 'El Capitan' Sierra 'High Sierra' Mojave)
 
 # show currently running version
@@ -8,4 +8,4 @@ installedMajorRelease=$(sw_vers -productVersion | awk -F '.' '{ print $2 }')
 echo "You're on ${OSProductPrefixList[$installedMajorRelease]} ${OSCodeNameList[$installedMajorRelease]}"
 
 # sanity check - show them all
-for i in `seq 0 13`; do echo "10.${i}: ${OSProductPrefixList[$i]} ${OSCodeNameList[$i]}" ; done
+for i in `seq 0 14`; do echo "10.${i}: ${OSProductPrefixList[$i]} ${OSCodeNameList[$i]}" ; done
