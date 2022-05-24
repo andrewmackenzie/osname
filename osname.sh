3fb2b28e68f4ade614967c55912ec4184315c61f
Andrew MacKenzie <andrew@capstannetworks.com>
2022-05-24 13:48:28 -0500

    Update osname.sh

--- osname.sh
+++ osname.sh
@@ -1,7 +1,7 @@
 # OS Release Lists
 # (No doubt there are more mature bash-y ways of doing this, but I'll understand this next time I look at it
-OSProductPrefixList=('Mac OS X' 'Mac OS X' 'Mac OS X' 'Mac OS X' 'Mac OS X' 'Mac OS X' 'Mac OS X' 'Mac OS X' 'OS X' 'OS X' 'OS X' 'OS X' macOS macOS macOS)
-OSCodeNameList=(Cheetah Puma Jaguar Panther Tiger Leopard 'Snow Leopard' Lion 'Mountain Lion' Mavericks Yosemite 'El Capitan' Sierra 'High Sierra' Mojave)
+OSProductPrefixList=('Mac OS X' 'Mac OS X' 'Mac OS X' 'Mac OS X' 'Mac OS X' 'Mac OS X' 'Mac OS X' 'Mac OS X' 'OS X' 'OS X' 'OS X' 'OS X' macOS macOS macOS macOS macOS macOS)
+OSCodeNameList=(Cheetah Puma Jaguar Panther Tiger Leopard 'Snow Leopard' Lion 'Mountain Lion' Mavericks Yosemite 'El Capitan' Sierra 'High Sierra' Mojave Catalina 'Big Sur' Monterey)
 
 # show currently running version
 installedMajorRelease=$(sw_vers -productVersion | awk -F '.' '{ print $2 }')
